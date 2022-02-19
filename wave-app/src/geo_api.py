139103ba6aec58ae036251149c29c362bfa209eb
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2022-02-19 23:13:03 +0530

    Enhance home UI

--- wave-app/src/geo_api.py
+++ wave-app/src/geo_api.py
@@ -1,9 +1,14 @@
+from http.client import responses
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
-
-def get_address(lat, lon):
-    url = f"https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json"
+def get_address(url):
+    # url = f"https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json"
     res = requests.get(url)
     address = res.json()
     return address["display_name"]
 
+def get_res(list_of_urls):
+    with ThreadPoolExecutor(max_workers=10) as pool:
+        response_list = list(pool.map(get_address,list_of_urls))
+        return response_list
--- wave-app/src/home.py
+++ wave-app/src/home.py
@@ -2,7 +2,7 @@ from h2o_wave import main, app, Q, ui
 import os
 from .ui_utils import make_markdown_table
 from .plot import *
-from .geo_api import get_address
+from .geo_api import get_address, get_res
 
 # Functions for data tab.
 
@@ -25,14 +25,23 @@ async def home(q:Q):
 
     df_values = df_head.values.tolist()
 
+    # Retrive the location name using the geo API
+    url_list = []
+
+    for i in range(0,10):
+        url = f"https://nominatim.openstreetmap.org/reverse?lat={df_values[i][0]}&lon={df_values[i][1]}&format=json"
+        url_list.append(url)
+
+    location_response = get_res(url_list)
+
     # Create columns for our issue table.
     columns = [
         ui.table_column(name='order', label='Order'),
-        ui.table_column(name='location', label='Location', min_width="400px" ),
+        ui.table_column(name='location', label='Location', min_width="450px" ),
         ui.table_column(name='latitude', label='Latitude' ),
         ui.table_column(name='longitude', label='Longitude'),
-        ui.table_column(name='confidence', label='Confidence', cell_type=ui.progress_table_cell_type()),
-        ui.table_column(name='est_risk', label='Estimates Risk', cell_type=ui.progress_table_cell_type()),
+        ui.table_column(name='confidence', label='Confidence', cell_type=ui.progress_table_cell_type(color="#FFFF00"), sortable=True),
+        ui.table_column(name='est_risk', label='Estimated Risk', cell_type=ui.progress_table_cell_type(color="#FF0000"), sortable=True),
     ]
 
     q.page['home'] = ui.form_card(box=ui.box('home'), items=[
@@ -40,11 +49,11 @@ async def home(q:Q):
         ui.text_m("Considering most recent 24 hours data captured by VIIRS satellite following regions shows higher fire rates"),
         # ui.text(df_table),
         ui.table(
-            name='issues',
+            name='stats',
             columns=columns,
             rows=[ui.table_row(
                 name=f'{i}',
-                cells=[str(i+1), get_address(df_values[i][0],df_values[i][1] ),str(df_values[i][0]), str(df_values[i][1]), str(df_values[i][4]/100), str(df_values[i][9])]
+                cells=[str(i+1), location_response[i],str(df_values[i][0]), str(df_values[i][1]), str(df_values[i][4]/100), str(df_values[i][9])]
             ) for i in range(0,10)],
             downloadable=True,
             # width='1000px',
@@ -53,78 +62,70 @@ async def home(q:Q):
 
     await q.page.save()
 
-    # q.page['map'] = ui.wide_pie_stat_card(
-    #     box=ui.box('map'),
-    #     title='Wide Pie Stat',
-    #     pies=[
-    #         ui.pie(label='Category 1', value='35%', fraction=0.35, color='#2cd0f5', aux_value='$ 35'),
-    #         ui.pie(label='Category 2', value='65%', fraction=0.65, color='$green', aux_value='$ 65'),
-    #     ]
-    # )
-
-    # await q.page.save()
-
-    # columns = 3
-    # rows = 2
-
-    # for column in range(1, columns + 1):
-    #     for row in range(1, rows + 1):
-    #         box = f'{column} {row} 1 1'
-    #         q.page[f'card_{column}_{row}'] = ui.markdown_card(box=box, title=box, content='')
-
-    # await q.page.save()
-
-    # q.page['map'] = ui.meta_card(box=ui.box('map'), items=[
-    #     ui.markdown_card(box='1 1 2 2', title="Hello", content="hhh")
-    # ])
-
     q.page['topic'] = ui.form_card(box=ui.box('predict'), items=[
-        ui.text_l("### High Fire Alert Regions for Last 24 Hours"),
+        ui.text_l("### More on High Fire Alert Regions"),
     ])
 
     for i in range(0,5):
-        q.page[f'card{i}'] = ui.form_card(box=ui.box('content1', width='250px', height='300px'),
+        q.page[f'card{i}'] = ui.form_card(box=ui.box('content1', width='250px', height='350px'),
                                         title='',
                                         items=[
-                                            ui.text_m("### SYDNEY"),
-                                            # ui.button(name='Sydney', label='SYDNEY', icon='Location', disabled=True),
+                                            ui.inline(items=[
+                                                ui.button(name='button', icon='Location', caption='Tooltip on hover', disabled=True),
+                                                ui.text_m(f'### {i+1}-{location_response[i].split(",")[0]}'),
+                                            ]),
                                             ui.separator(),
-                                            # ui.button(name='standard_disabled_button', label='### Standard', disabled=True),
-                                            ui.text_m("Tot Fire Count  : "),
-                                            ui.text_m("Est Fire Area  :"),
-                                            ui.text_m("Power(MegaWatt)  :"),
-                                            ui.text_m("Brightness : "),
+                                            ui.inline(items=[
+                                                ui.text_s('Total Fire Count'),
+                                                ui.text_l(f'{str(df_values[i][3])}'),
+                                            ]),
+                                            ui.inline(items=[
+                                                ui.text_s('Estimated Fire Area'),
+                                                ui.text_l(f'{str(df_values[i][7])}'),
+                                            ]),
+                                            ui.inline(items=[
+                                                ui.text_s('Power(MegaWatt)'),
+                                                ui.text_l(f'{str(df_values[i][6])}'),
+                                            ]),
+                                             ui.inline(items=[
+                                                ui.text_s('Brightness'),
+                                                ui.text_l(f'{str(df_values[i][5])}'),
+                                            ]),
                                             ui.separator(),
-                                            ui.slider(name='slider_disabled', label='Confidence', min=0, max=100, step=10, value=30,
+                                            ui.slider(name='slider_disabled', label='Confidence', min=0, max=100, step=10, value=df_values[i][4],
                                             disabled=True),
                                         ],
                                         )
 
-    # for i in range(5,10):
-    #     q.page[f'card{i}'] = ui.form_card(box=ui.box('content2', width='250px', height='200px'), 
-    #                                     title='',
-    #                                     items=[
-    #                                         ui.text_m("### High Fire Alert Regions for Last 24 Hours"),
-    #                                     ])
-
-    
-    # q.page['card1'] = ui.tall_info_card(box=ui.box('content', width='200px', height='200px'), name='', 
-    #                                     title='Card', caption='Lorem ipsum')
-    # q.page['card2'] = ui.tall_info_card(box=ui.box('content', width='200px', height='200px'), name='',
-    #                                     title='Card', caption='Lorem ipsum')
-    # q.page['card3'] = ui.tall_info_card(box=ui.box('content', width='200px', height='200px'), name='',
-    #                                     title='Card', caption='Lorem ipsum')
-    # q.page['card4'] = ui.tall_info_card(box=ui.box('content', width='200px', height='200px'), name='',
-    #                                     title='Card', caption='Lorem ipsum')
-    # q.page['card5'] = ui.tall_info_card(box=ui.box('content', width='200px', height='200px'), name='',
-    #                                     title='Card', caption='Lorem ipsum')
-    # q.page['card6'] = ui.tall_info_card(box=ui.box('content', width='200px', height='200px'), name='',
-    #                                     title='Card', caption='Lorem ipsum')                                    
-                                        
-    # q.page['quote'] = ui.markdown_card(
-    #     box=ui.box('content'),
-    #     title='Hello World',
-    #     content='"The Internet? Is that thing still around?" - *Homer Simpson*',
-    # )
+    for i in range(5,10):
+        q.page[f'card{i}'] = ui.form_card(box=ui.box('content2', width='250px', height='350px'),
+                                        title='',
+                                        items=[
+                                            ui.inline(items=[
+                                                ui.button(name='button', icon='Location', caption='Tooltip on hover', disabled=True),
+                                                ui.text_m(f'### {i+1}-{location_response[i].split(",")[0]}'),
+                                            ]),
+                                            ui.separator(),
+                                            ui.inline(items=[
+                                                ui.text_s('Total Fire Count'),
+                                                ui.text_l(f'{str(df_values[i][3])}'),
+                                            ]),
+                                            ui.inline(items=[
+                                                ui.text_s('Estimated Fire Area'),
+                                                ui.text_l(f'{str(df_values[i][7])}'),
+                                            ]),
+                                            ui.inline(items=[
+                                                ui.text_s('Power(MegaWatt)'),
+                                                ui.text_l(f'{str(df_values[i][6])}'),
+                                            ]),
+                                             ui.inline(items=[
+                                                ui.text_s('Brightness'),
+                                                ui.text_l(f'{str(df_values[i][5])}'),
+                                            ]),
+                                            ui.separator(),
+                                            ui.slider(name='slider_disabled', label='Confidence', min=0, max=100, step=10, value=df_values[i][4],
+                                            disabled=True),
+                                        ],
+                                        )
 
     await q.page.save()
