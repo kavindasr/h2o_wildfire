d7e7c3a5be0e28c82f2e6b4cf34f6538cb225b3e
Dasith U Edirisinghe <udasith@gmail.com>
2022-02-21 22:11:14 +0530

    predict.py addded

--- wave-app/src/utils/__init__.py
+++ wave-app/src/utils/__init__.py
@@ -1,6 +1,5 @@
 from .data_preparation import *
-<<<<<<< Updated upstream
 from .filter_location import *
-=======
 from .data_preparation_v2 import *
->>>>>>> Stashed changes
+from .predict import *
+
