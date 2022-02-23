9a96940f1ff9605357a846cedfe310f23730ab7c
Dasith U Edirisinghe <udasith@gmail.com>
2022-02-23 23:11:48 +0530

    filter_location.py updated

--- wave-app/src/utils/filter_location.py
+++ wave-app/src/utils/filter_location.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from datetime import date
+
 
 def filterLocation(aus_fires,latitude,longitude):
     ## aus_fires = DataFrame
@@ -18,9 +20,15 @@ def main(aus_fires,lat,lng):
     df_w.set_index('ds', inplace=True)
     df_w.index = pd.to_datetime(df_w.index)
     df_w = df_w.resample('1W').mean()
-
     df_w['y'] = df_w['y'].fillna(0)
 
-    df_w.reset_index('ds', inplace=True)
+    # Add missing weeks
+    max_date = df.ds.max()
+    end = date.today()
+    if (max_date<end):
+        r = pd.date_range(start=df.ds.min(), end=end,freq='W')
+        df_w = df_w.reindex(r).fillna(0.0).rename_axis('ds').reset_index()
+    else:
+        df_w.reset_index('ds', inplace=True)
 
     return df_w
